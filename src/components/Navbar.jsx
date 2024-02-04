5657a75d052104e04bd806391f1a88ce9856d286
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-04 18:26:15 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import {AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai'
+import {AiOutlineClose, AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai'
 import {BsFillCartFill} from "react-icons/bs";
 
 const Navbar = () => {
     return (
         <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
             {/* Left side */}
+
             <div className='flex items-center'>
                 <div className='cursor-pointer'>
                     <AiOutlineMenu size={30}/>
@@ -33,6 +34,18 @@ const Navbar = () => {
             <button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>
                 <BsFillCartFill size={20} className='mr-2'/> Cart
             </button>
+
+            {/*    Mobile Menu   */}
+            {/* Overlay */}
+            <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+
+        {/*    Side drawer Menu   */}
+            <div className=' fixed top-0 left-0 w-[300px] h-screen bg-white
+            z-10 duration-300'>
+                <AiOutlineClose size={30} className='absolute right-4 top-4 cursor-pointer' />
+                <h2 className='text-2xl py-4'>Best <span className='font-bold'>Eats</span></h2>
+            </div>
+
         </div>
     )
 }

15d67e2abe6f79dd654c2442aaa1a2d270b9b777
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-04 18:12:17 +0530

    search bar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineSearch} from 'react-icons/ai'
 
 const Navbar = () => {
     return (
@@ -9,12 +9,23 @@ const Navbar = () => {
                 <div className='cursor-pointer'>
                     <AiOutlineMenu size={30}/>
                 </div>
-                <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2 '>Best <span>Eats</span>
+                <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2 '>Best <span className='font-bold'>Eats</span>
                 </h1>
-                <div>
-                    <p>Delivery</p>
-                    <p>Pickup</p>
+                <div className='hidden lg:flex items-center bg-gray-200
+                rounded-full p-1 text-[14px]'>
+                    <p className='bg-black text-white rounded-full p-2'>Delivery</p>
+                    <p className='p-2'>Pickup</p>
                 </div>
+            </div>
+
+            {/*     Search Input  */}
+            <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+                <AiOutlineSearch size={25}/>
+                <input
+                    className='bg-transparent p-2 w-full focus:outline-none'
+                    type='text'
+                    placeholder='Search foods'
+                />
             </div>
         </div>
     )

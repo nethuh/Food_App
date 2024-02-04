183b84da7d0b4969671c5b1df5ad2f7b956aa457
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-05 02:31:41 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div>
-<div>
-{/*    Overlay  */}
-    <div>
-        <h1>The <span>Best</span></h1>
-        <h1>Foods <span>Delivered</span></h1>
-    </div>
-</div>
+        <div className='max-w-[1640px] mx-auto p-4'>
+            <div className='max-h-[500px] relative'>
+                {/*    Overlay  */}
+                <div className='absolute w-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+                    <h1>The <span>Best</span></h1>
+                    <h1>Foods <span>Delivered</span></h1>
+                </div>
+                <img src="https://img.freepik.com/free-photo/delicious-meal-table_23-2150857956.jpg?size=626&ext=jpg&uid=R94049744&ga=GA1.1.1536149295.1697625185&semt=ais_ai_generated"/>
+            </div>
         </div>
     );
 };

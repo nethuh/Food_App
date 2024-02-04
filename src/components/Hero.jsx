825c353125c8cb6ebaf82662c80a893b73377b49
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-05 02:18:23 +0530

    hero section added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div>
+<div>
+{/*    Overlay  */}
+    <div>
+        <h1>The <span>Best</span></h1>
+        <h1>Foods <span>Delivered</span></h1>
+    </div>
+</div>
+        </div>
+    );
+};
+
+export default Hero;

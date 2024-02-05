1948f098db37f7d450da364565b7246e16e6cf07
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-05 16:21:11 +0530

    fliter type and price added

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import {data} from '../data/data'
+import React, {useState} from "react";
+import {categories, data} from '../data/data'
 
 const Food = () => {
-    console.log(data)
+    // console.log(data)
+    const [foods, setFoods] = useState(data);
+
+    //   Filter Type burgers/pizza/etc
+    const filterType = (category) => {
+        setFoods(
+            data.filter((item) => {
+                return item.category === category;
+            })
+        );
+    };
+
+    //   Filter by price
+    const filterPrice = (price) => {
+        setFoods(
+            data.filter((item) => {
+                return item.price === price;
+            })
+        );
+    };
     return (
         <div className='max-w-[1640px] m-auto px4 py-12'>
             <h1 className='text-orange-600 font-bold text-4xl text-center'>
@@ -16,11 +35,21 @@ const Food = () => {
                 <div>
                     <p className='font-bold text-gray-700'>Filter Type</p>
                     <div className='flex justify-between flex-wrap'>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All
+                        </button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers
+                        </button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza
+                        </button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads
+                        </button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken
+                        </button>
                     </div>
                 </div>
 
@@ -28,12 +57,39 @@ const Food = () => {
                 <div>
                     <p className='font-bold text-gray-700'>Filter Price</p>
                     <div className='flex justify-between max-w-[390px] w-full'>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
-                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$
+                        </button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$
+                        </button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$
+                        </button>
+                        <button
+                            className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$
+                        </button>
                     </div>
                 </div>
+            </div>
+
+            {/*    Display Food  */}
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+                {foods.map((item, index) => (
+                    <div key={index} className='border rounded-lg shadow-lg hover:scale-105 duration-300'>
+                        <img src={item.image} alt={item.name}
+                             className='w-full h-[200px] object-cover rounded-t-lg'
+                        />
+                        <div className='flex justify-between px-2 py-4'>
+                            <p className='font-bold'>{item.name}</p>
+                            <p>
+                    <span className='bg-orange-500 text-white p-1 rounded-full'>
+                        {item.price}
+                    </span>
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     );

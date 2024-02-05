059585cef680c7c6a82e301e4c7226691f603815
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-05 14:31:40 +0530

    card sectio done

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -12,7 +12,38 @@ const HeadlineCards = () => {
                     <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
                 </div>
                 <img
-                    className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://img.freepik.com/free-photo/grilled-cheeseburger-sesame-bun-with-fresh-toppings-generative-ai_188544-12325.jpg?size=626&ext=jpg&uid=R94049744&ga=GA1.2.1536149295.1697625185&semt=sph'/>
+                    className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+                    src='https://img.freepik.com/free-photo/grilled-cheeseburger-sesame-bun-with-fresh-toppings-generative-ai_188544-12325.jpg?size=626&ext=jpg&uid=R94049744&ga=GA1.2.1536149295.1697625185&semt=sph'/>
+            </div>
+
+            {/* Card */}
+            <div className='rounded-xl relative'>
+                {/* Overlay */}
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
+                    <p className='px-2'>Added Daily</p>
+                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+                </div>
+                <img
+                    className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+                    src='https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+                    alt='/'
+                />
+            </div>
+
+            {/* Card */}
+            <div className='rounded-xl relative'>
+                {/* Overlay */}
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
+                    <p className='px-2'>Tasty Treats</p>
+                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+                </div>
+                <img
+                    className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+                    src='https://images.unsplash.com/photo-1559715745-e1b33a271c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRlc3NlcnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
+                    alt='/'
+                />
             </div>
         </div>
     );

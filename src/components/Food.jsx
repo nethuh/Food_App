2b2f3914b8b98b26035689b5df9d5cbdbf295bb9
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-05 16:05:37 +0530

    updated

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -16,22 +16,22 @@ const Food = () => {
                 <div>
                     <p className='font-bold text-gray-700'>Filter Type</p>
                     <div className='flex justify-between flex-wrap'>
-                        <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
-                        <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
-                        <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
-                        <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
-                        <button className='border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>All</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Salads</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>Chicken</button>
                     </div>
                 </div>
 
                 {/*    Filter Price  */}
                 <div>
-                    <p>Filter Price</p>
-                    <div>
-                        <button>$</button>
-                        <button>$$</button>
-                        <button>$$$</button>
-                        <button>$$$$</button>
+                    <p className='font-bold text-gray-700'>Filter Price</p>
+                    <div className='flex justify-between max-w-[390px] w-full'>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
+                        <button className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
                     </div>
                 </div>
             </div>

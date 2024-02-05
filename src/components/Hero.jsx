604b47ed799e4e78f1203cbc69fcd01924663de9
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-02-05 14:05:37 +0530

    Hero sectio done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,15 @@ const Hero = () => {
         <div className='max-w-[1640px] mx-auto p-4'>
             <div className='max-h-[500px] relative'>
                 {/*    Overlay  */}
-                <div className='absolute w-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
-                    <h1>The <span>Best</span></h1>
-                    <h1>Foods <span>Delivered</span></h1>
+                <div
+                    className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span
+                        className='text-orange-500'>Best</span></h1>
+                    <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span
+                        className='text-orange-500'>Foods</span> Devlivered</h1>
                 </div>
-                <img src="https://img.freepik.com/free-photo/delicious-meal-table_23-2150857956.jpg?size=626&ext=jpg&uid=R94049744&ga=GA1.1.1536149295.1697625185&semt=ais_ai_generated"/>
+                <img className='w-full max-h-[500px] object-cover'
+                    src="https://img.freepik.com/free-photo/delicious-meal-table_23-2150857956.jpg?size=626&ext=jpg&uid=R94049744&ga=GA1.1.1536149295.1697625185&semt=ais_ai_generated"/>
             </div>
         </div>
     );
